8448541587c7aca393606c44b19b1aef21c9ce65
Chantal <chantalgomez@pursuit.org>
2023-08-08 22:58:23 -0400

    updates

--- controllers/playersController.js
+++ controllers/playersController.js
@@ -17,7 +17,7 @@ router.get("/all-players-in-team", async (req, res) => {
   }
 });
 
-  // localhost:3006/teams/1/players/7
+  // localhost:3006/teams/1/players/74
 router.get("/:playerId", async (req, res) => {
   try {
     const player = await getPlayerById(
